92eed12b47da24359e14c96da933f87e47f5427a
Александр Щукин <krhuda@yandex.ru>
2020-07-17 14:10:02 +0000

    Example

---
+++ edabit puzzles/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Learning_projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...\n");
+            Console.ReadKey(true);
+        }
+    }
+}
