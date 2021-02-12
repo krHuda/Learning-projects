9e36c4cb8565a8e1fa2e51ef459813cb712fcdfc
SeriousIgor <seriousigorok@gmail.com>
2021-02-13 01:10:57 +0200

    Added GetSize method, modified Game class (it can be ended now), added Size property for incapsulation size field, put GetSize method into StarGame method

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -3,20 +3,46 @@ namespace TicTacToe
 {
     class Field
     {
-        int size;
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            private set
+            {
+                size = value;
+            }
+        }
+        int size = 0;
         bool move;
         int[,] cells = new int[10, 10];
-        public Field(int s)
-        {
-            size = s;
-        }
-        public void StartGame()
+        public void StartGame()     //Начало игры
         {
             move = true;
+            GetSize();
             SetNull();
             DrawField();
         }
-        void DrawField()
+        void GetSize()              //Получить размер поля; проверить на целое число больше 3.
+        {
+            string s;
+            bool isNum = false;
+            while (!isNum)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the game\nWrite field size: ");
+                s = Console.ReadLine();
+                isNum = int.TryParse(s, out size);
+                if (size < 3)
+                {
+                    isNum = false;
+                    Console.WriteLine("Your digit is too small or not simple.\nPress any key to continue.");
+                    Console.ReadKey();
+                }
+            }
+        }
+        void DrawField()           //Рисует игровое поле
         {
             Console.Clear();
             for (int i = 0; i < size; i++)
@@ -38,10 +64,11 @@ namespace TicTacToe
                 }
             }
         }
-        public void MakeMove(int i, int j)
+        public void MakeMove(int pos)
         {
-            i--;
-            j--;
+            int i, j;
+            i = (pos / 10) - 1;
+            j = (pos % 10) - 1;
             if (move)
                 cells[i, j] = 1;
             else
@@ -54,19 +81,18 @@ namespace TicTacToe
     {
         static void Main()
         {
-            int size;
-            int i, j;
-            Console.WriteLine("Welcome to the game\nWrite field size: ");
-            size = Convert.ToInt32(Console.ReadLine());
-            Field f = new Field(size);
+            int pos;
+            int i = 0;
+            Field f = new Field();
             f.StartGame();
-            while (true)
+            while (i < (f.Size * f.Size))
             {
                 Console.WriteLine("Enter position: ");
-                i = Convert.ToInt32(Console.ReadLine());
-                j = Convert.ToInt32(Console.ReadLine());
-                f.MakeMove(i, j);
+                pos = Convert.ToInt32(Console.ReadLine());
+                f.MakeMove(pos);
+                i++;
             }
+            Console.WriteLine("Thanks for the game :)");
         }
     }
 }
