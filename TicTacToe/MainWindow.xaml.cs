afa81539aff1be7fb75a9b67a0614c201729d5ee
krHuda <krHuda@yandex.ru>
2022-07-07 17:06:08 +0300

    Сделал всплывающую подсказку на кнопке рестарта.

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using System.Windows.Media;
 using TicTacToe.Additions;
 using TicTacToe.Game;
